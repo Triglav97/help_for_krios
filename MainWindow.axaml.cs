83febd81cdfddbcc69d2a369b54373821fdf1240
kriosereb <kriosereb@gmail.com>
2021-04-06 00:38:23 +0300

    Final

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,5 +1,3 @@
-using System.Windows;
-
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
@@ -7,25 +5,27 @@ using Avalonia.Interactivity;
 using MessageBox.Avalonia;
 
 namespace Hello {
-	
-	public partial class MainWindow : Window {
+	public class MainWindow : Window {
 		public MainWindow() {
-			this.InitializeComponent();
-			
-// #if DEBUG
-// 			this.AttachDevTools();
-// #endif
+			InitializeComponent();
+#if DEBUG
+			this.AttachDevTools();
+#endif
 		}
 
-		private void Button_Click(object sender, RoutedEventArgs e) {
+		public void button_Click(object sender, RoutedEventArgs e) {
 			var userName = this.FindControl<TextBox>("userName");
-			var msg = MessageBoxManager.GetMessageBoxStandardWindow("title", "Hello " + userName.Text);
-			msg.Show();
+			if (userName.Text != null) {
+				var msg = MessageBoxManager.GetMessageBoxStandardWindow("title", "Hello " + userName.Text);
+				msg.Show();
+			} else {
+				var msg = MessageBoxManager.GetMessageBoxStandardWindow("Error", "Name is empty!");
+				msg.Show();
+			}
 		}
 
 		private void InitializeComponent() {
 			AvaloniaXamlLoader.Load(this);
 		}
 	}
-
 }
